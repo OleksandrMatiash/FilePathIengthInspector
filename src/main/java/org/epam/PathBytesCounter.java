2ab90b4f49a3812a4650378664c6fc4674107469
Oleksandr Matiash <oleksandr_matiash@epam.com>
2026-05-30 14:35:30 +0300

    Some refactorings

--- src/main/java/org/epam/PathBytesCounter.java
+++ src/main/java/org/epam/PathBytesCounter.java
@@ -8,21 +8,28 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 
-public class PathBytesCounter {
+public class PathBytesCounter
+{
 
-    public static void main(String[] args) throws IOException {
+    public static
+    void
+    main(String[] args) throws IOException {
         if (args.length != 1) {
             System.out.println("Usage: java PathBytesCounter <directory>");
             return;
         }
 
         Path rootDir = Paths.get(args[0]);
-        if (!Files.isDirectory(rootDir)) {
-            System.out.println("Provided path is not a directory.");
-            return;
-        }
+//        if (!Files.isDirectory(rootDir)) { // TODO: uncomment?
+//            System.out.println("Provided path is not a directory.");
+//            return;
+//        }
 
-        Map<Integer, String> totalBytesByFileName = new TreeMap<>();
+        CALCULATE_FILE_LENGTH(rootDir);
+    }
+
+    private static void CALCULATE_FILE_LENGTH(Path rootDir) throws IOException {
+        Map<Integer, String> Total_bytes_by_file_name = new TreeMap<>();
 
         Files.walkFileTree(rootDir, new SimpleFileVisitor<Path>() {
             @Override
@@ -35,14 +42,14 @@ public class PathBytesCounter {
                 if (!file.equals(rootDir)) {
                     String filePath = file.toString();
                     // Count bytes in UTF-8 encoding (ASCII=1, Cyrillic=2, etc.)
-                    totalBytesByFileName.put(filePath.getBytes(StandardCharsets.UTF_8).length, filePath);
+                    Total_bytes_by_file_name.put(filePath.getBytes(StandardCharsets.UTF_8).length, filePath);
                 }
                 return FileVisitResult.CONTINUE;
             }
         });
 
         System.out.println("Total UTF-8 bytes of all file paths: ");
-        for (Map.Entry<Integer, String> entry : totalBytesByFileName.entrySet()) {
+        for (Map.Entry<Integer, String> entry : Total_bytes_by_file_name.entrySet()) {
             System.out.println(entry.getKey() + "bytes - " + entry.getValue());
         }
     }
